35f7a8c2b056f5f8e7de6a9fd36172343e2c1f11
lisawilliams <lisa@placeblogger.com>
2018-04-03 21:52:49 -0400

    adds all 3 scenes and animations WITHOUT looping -- good for showing how it works in a simple way. Next step: use a loop to do all 3 in one code block.

--- 01-intro/start/js/main.js
+++ 01-intro/start/js/main.js
@@ -6,13 +6,48 @@ $(document).ready(function(){
 	//build a scene
 	var ourScene = new ScrollMagic.Scene ({
 		triggerElement: '#project01 img', // adding img makes the fade scene start at the top of the image
-		duration: '60%', // makes it fade out at 60% of the viewport, so it fades as you scroll
+		// duration: '60%', // makes it fade out at 60% of the viewport, so it fades as you scroll
 		triggerHook: 0.9,
-
+		reverse: false  // means animation only happens once, not when users scroll back up the screen
 	})
 	.setClassToggle('#project01', 'fade-in')
 	.addIndicators({
 		name: 'fade scene',
+		colorTrigger: 'black',
+		indent: 50,
+		colorStart: 'green',
+		colorEnd: 'red',
+	}) // this requires the plugin made available in index.html
+	// addIndicators allows us to see indicators as we scroll for debugging purposes
+	.addTo(controller);
+
+	var ourScene = new ScrollMagic.Scene ({
+		triggerElement: '#project02 img', // adding img makes the fade scene start at the top of the image
+		// duration: '60%', // makes it fade out at 60% of the viewport, so it fades as you scroll
+		triggerHook: 0.9,
+		reverse: false  // means animation only happens once, not when users scroll back up the screen
+	})
+	.setClassToggle('#project02', 'fade-in')
+	.addIndicators({
+		name: 'fade scene 2',
+		colorTrigger: 'black',
+		indent: 50,
+		colorStart: 'green',
+		colorEnd: 'red',
+	}) // this requires the plugin made available in index.html
+	// addIndicators allows us to see indicators as we scroll for debugging purposes
+	.addTo(controller);
+
+
+	var ourScene = new ScrollMagic.Scene ({
+		triggerElement: '#project03 img', // adding img makes the fade scene start at the top of the image
+		// duration: '60%', // makes it fade out at 60% of the viewport, so it fades as you scroll
+		triggerHook: 0.9,
+		reverse: false  // means animation only happens once, not when users scroll back up the screen
+	})
+	.setClassToggle('#project03', 'fade-in')
+	.addIndicators({
+		name: 'fade scene 3',
 		colorTrigger: 'black',
 		indent: 50,
 		colorStart: 'green',
