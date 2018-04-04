4cef5cb74344983101a124893b848c6e24bc6c1d
lisawilliams <lisa@placeblogger.com>
2018-04-03 21:41:57 -0400

    adding code that does fade in-fade out elements and triggers

--- 01-intro/start/js/main.js
+++ 01-intro/start/js/main.js
@@ -1,23 +1,24 @@
 $(document).ready(function(){
 
-	
+	// Init ScrollMagic
+	var controller = new ScrollMagic.Controller();
+
+	//build a scene
+	var ourScene = new ScrollMagic.Scene ({
+		triggerElement: '#project01 img', // adding img makes the fade scene start at the top of the image
+		duration: '60%', // makes it fade out at 60% of the viewport, so it fades as you scroll
+		triggerHook: 0.9,
+
+	})
+	.setClassToggle('#project01', 'fade-in')
+	.addIndicators({
+		name: 'fade scene',
+		colorTrigger: 'black',
+		indent: 50,
+		colorStart: 'green',
+		colorEnd: 'red',
+	}) // this requires the plugin made available in index.html
+	// addIndicators allows us to see indicators as we scroll for debugging purposes
+	.addTo(controller);
 
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
